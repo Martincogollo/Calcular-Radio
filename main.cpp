efada46d4a887c55204e00eef4a611bf05c6eaa6
Martin <x@x.com>
2022-12-05 11:59:03 -0500

    Calcular el Radio

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+int main() {
+ float radio=0, A_circuferencia=0;
+
+ cout<<"\ndigite el valor del radio: ";
+ cin>>radio;
+
+ A_circuferencia= 3.1416 * pow(radio,2);
+
+  cout<<"\nel area de la circuferencia es: "<<A_circuferencia<<endl;
+
+
+
+
+  return 0;
+}
